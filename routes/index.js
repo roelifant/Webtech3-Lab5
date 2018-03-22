6345c0b8c8646d67a8cd921bc1a62c3e752b2889
Roel <roelifant@gmail.com>
2018-03-22 12:17:18 +0100

    added routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/createpoll', function(req, res, next) {
+  res.render('createpoll');
+});
+
 module.exports = router;
